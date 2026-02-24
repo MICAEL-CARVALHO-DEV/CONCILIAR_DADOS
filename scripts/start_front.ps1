941a53d08c5512fa8a351b05dcae1cacff990de1
MICAEL-CARVALHO-DEV <micaelcarvalho003@gmail.com>
2026-02-24 16:55:20 -0300

    chore: add one-click startup script and fix Windows front launcher

--- scripts/start_front.ps1
+++ scripts/start_front.ps1
@@ -1,7 +1,8 @@
-$ErrorActionPreference = "Stop"
+﻿$ErrorActionPreference = "Stop"
 
 $root = Split-Path -Parent $PSScriptRoot
 Set-Location $root
 
 Write-Host "[front] iniciando servidor em http://127.0.0.1:5500"
-python -m http.server 5500 --bind 127.0.0.1
+py -3 -m http.server 5500 --bind 127.0.0.1
+
